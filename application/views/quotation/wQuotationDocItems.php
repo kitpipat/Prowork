1ae463ed6aa9c9705f0d6a297cb1b86d292a70b1
Supawat Wat. Putayanan <Supawat@adasoft.adasoft.com>
2020-04-23 20:10:59 +0700

    wQution

--- application/views/quotation/wQuotationDocItems.php
+++ application/views/quotation/wQuotationDocItems.php
@@ -1,89 +1,88 @@
-
 <table class="table table-striped">
-  <tr>
-    <th>ลำดับ</th>
-    <th>ลบ</th>
-    <th>รูปภาพ</th>
-    <th>รายการ</th>
-    <th>หน่วย</th>
-    <?php
-    if($tSesUserGroup == 4){?>
-    <th>ผู้จำหน่าย</th>
-    <th>ต้นทุน</th>
-    <?php } ?>
-    <th>ราคา/หน่วย</th>
-    <th  style="width:80px;">จำนวน</th>
-    <th>จำนวนเงิน</th>
-    <th  style="width:80px;">ส่วนลด</th>
-    <th>จำนวนเงินรวม</th>
-  </tr>
-  <?php
-      if($aDocItems["nTotalRes"] > 0) {
-  ?>
-  <?php
+	<tr>
+		<th>ลำดับ</th>
+		<th>ลบ</th>
+		<th>รูปภาพ</th>
+		<th>รายการ</th>
+		<th>หน่วย</th>
+		<?php
+		if ($tSesUserGroup == 4) { ?>
+			<th>ผู้จำหน่าย</th>
+			<th>ต้นทุน</th>
+		<?php } ?>
+		<th>ราคา/หน่วย</th>
+		<th style="width:80px;">จำนวน</th>
+		<th>จำนวนเงิน</th>
+		<th style="width:80px;">ส่วนลด</th>
+		<th>จำนวนเงินรวม</th>
+	</tr>
+	<?php
+	if ($aDocItems["nTotalRes"] > 0) {
+	?>
+		<?php
 
-       $nTotal = 0;
-       $nPdtNetTotal = 0;
-       $nDocNetTotal = 0;
-       $nXqdDis = 0;
-       for($p = 0;$p< $aDocItems["nTotalRes"] ;$p++){
+		$nTotal = 0;
+		$nPdtNetTotal = 0;
+		$nDocNetTotal = 0;
+		$nXqdDis = 0;
+		for ($p = 0; $p < $aDocItems["nTotalRes"]; $p++) {
 
-            $tPdtCode = $aDocItems["raItems"][$p]["FTPdtCode"];
-            $tPdtName = $aDocItems["raItems"][$p]["FTPdtName"];
-            $tPunName = $aDocItems["raItems"][$p]["FTPunName"];
-            $nXqdUnitPrice = $aDocItems["raItems"][$p]["FCXqdUnitPrice"];
-            $nXqdQty = $aDocItems["raItems"][$p]["FCXqdQty"];
-            $nTotal = $nXqdQty * $nXqdUnitPrice;
-            $nXqdDis = $aDocItems["raItems"][$p]["FCXqdDis"];
-            $nXqdCost = $aDocItems["raItems"][$p]["FTXqdCost"];
-            $tSplName = $aDocItems["raItems"][$p]["FTSplName"];
-
-
-            if($nXqdDis == ""){
-               $nXqdDis = 0;
-            }else{
-               $nXqdDis = $nXqdDis;
-            }
-
-            $tDisType = substr($nXqdDis,strlen($nXqdDis)-1);
-
-            if($tDisType == ""){
-
-               $nPdtNetTotal = $nTotal;
-
-            }else if($tDisType == "%"){
-
-                $nPdtNetTotal = $nTotal - (($nTotal * substr($nXqdDis,0,strlen($nXqdDis)-1))/100);
-
-            }else{
-
-                $nPdtNetTotal = $nTotal - $nXqdDis;
-            }
-
-            $nDocNetTotal = $nDocNetTotal + $nPdtNetTotal;
+			$tPdtCode = $aDocItems["raItems"][$p]["FTPdtCode"];
+			$tPdtName = $aDocItems["raItems"][$p]["FTPdtName"];
+			$tPunName = $aDocItems["raItems"][$p]["FTPunName"];
+			$nXqdUnitPrice = $aDocItems["raItems"][$p]["FCXqdUnitPrice"];
+			$nXqdQty = $aDocItems["raItems"][$p]["FCXqdQty"];
+			$nTotal = $nXqdQty * $nXqdUnitPrice;
+			$nXqdDis = $aDocItems["raItems"][$p]["FCXqdDis"];
+			$nXqdCost = $aDocItems["raItems"][$p]["FTXqdCost"];
+			$tSplName = $aDocItems["raItems"][$p]["FTSplName"];
 
 
-  ?>
-  <tr>
-    <td><?=$p?></td>
-    <td>[x]</td>
-    <td><img src='<img src="<?=base_url('application/assets/images/products/NoImage.png') ?>" alt="...">'></td>
-    <td> <?php echo $tPdtCode." - ".$tPdtName; ?> </td>
-    <td> <?php echo $tPunName;?> </td>
-    <?php if($tSesUserGroup == 4){?>
-    <td><?php echo $tSplName;?></td>
-    <td><?php echo number_format($nXqdCost,2);?></td>
-    <?php } ?>
-    <td class="text-right"> <?php echo number_format($nXqdUnitPrice,2); ?> </td>
-    <td><input type="text" class="text-right" name="" value="<?=$nXqdQty?>" style="width:80px;"></td>
-    <td class="text-right"> <?php echo number_format($nTotal,2); ?> </td>
-    <td> <input type="text"class="text-right" name="" value="<?=$nXqdDis?>" style="width:80px;"> </td>
-    <td class="text-right"><?php echo number_format($nPdtNetTotal,2);?></td>
-  </tr>
-  <?php } ?>
-<?php }else{ ?>
-   <tr><td colspan="10">-ไม่พบรายการสินค้าในเอกสาร-</td></tr>
-<?php } ?>
+			if ($nXqdDis == "") {
+				$nXqdDis = 0;
+			} else {
+				$nXqdDis = $nXqdDis;
+			}
+
+			$tDisType = substr($nXqdDis, strlen($nXqdDis) - 1);
+
+			if ($tDisType == "") {
+
+				$nPdtNetTotal = $nTotal;
+			} else if ($tDisType == "%") {
+
+				$nPdtNetTotal = $nTotal - (($nTotal * substr($nXqdDis, 0, strlen($nXqdDis) - 1)) / 100);
+			} else {
+
+				$nPdtNetTotal = $nTotal - $nXqdDis;
+			}
+
+			$nDocNetTotal = $nDocNetTotal + $nPdtNetTotal;
+
+
+		?>
+			<tr>
+				<td><?= $p ?></td>
+				<td>[x]</td>
+				<td><img src='<img src="<?= base_url('application/assets/images/products/NoImage.png') ?>" alt="...">'></td>
+				<td> <?php echo $tPdtCode . " - " . $tPdtName; ?> </td>
+				<td> <?php echo $tPunName; ?> </td>
+				<?php if ($tSesUserGroup == 4) { ?>
+					<td><?php echo $tSplName; ?></td>
+					<td><?php echo number_format($nXqdCost, 2); ?></td>
+				<?php } ?>
+				<td class="text-right"> <?php echo number_format($nXqdUnitPrice, 2); ?> </td>
+				<td><input type="text" class="text-right" name="" value="<?= $nXqdQty ?>" style="width:80px;"></td>
+				<td class="text-right"> <?php echo number_format($nTotal, 2); ?> </td>
+				<td> <input type="text" class="text-right" name="" value="<?= $nXqdDis ?>" style="width:80px;"> </td>
+				<td class="text-right"><?php echo number_format($nPdtNetTotal, 2); ?></td>
+			</tr>
+		<?php } ?>
+	<?php } else { ?>
+		<tr>
+			<td colspan="10">-ไม่พบรายการสินค้าในเอกสาร-</td>
+		</tr>
+	<?php } ?>
 </table>
 
-<span id="ospDocNetTotal" style="display:none"><?php echo $nDocNetTotal;?></span>
+<span id="ospDocNetTotal" style="display:none"><?php echo $nDocNetTotal; ?></span>
--- application/views/quotation/wQuotationPdtList.php
+++ application/views/quotation/wQuotationPdtList.php
@@ -1,85 +1,86 @@
-<?php
-if($nTotalRecord > 0){
-    //echo $nTotalRecord;
-    if($tPdtViewType == 1) {
-?>
-                <?php
-                     //var_dump($aPdtList['raItems']);
-                     for($p=0;$p<$nTotalRecord;$p++){
+<?php if ($nTotalRecord > 0) { ?>
+	<?php if ($tPdtViewType == 1) { ?>
+			<?php
+			for ($p = 0; $p < $nTotalRecord; $p++) {
 
-                      $tPdtCode = $aPdtList['raItems'][$p]['FTPdtCode'];
-                      $tPdtName = $aPdtList['raItems'][$p]['FTPdtName'];
-                      $tPunCode = $aPdtList['raItems'][$p]['FTPunCode'];
-                      $tSplCode = $aPdtList['raItems'][$p]['FTSplCode'];
-                      $nPdtCost = $aPdtList['raItems'][$p]['FCPdtCostAFDis'];
-                      $nPdtUnitPri = $aPdtList['raItems'][$p]['FCPdtNetSalPri'];
-                      $tPunName = $aPdtList['raItems'][$p]['FTPunName'];
+				$tPdtCode = $aPdtList['raItems'][$p]['FTPdtCode'];
+				$tPdtName = $aPdtList['raItems'][$p]['FTPdtName'];
+				$tPunCode = $aPdtList['raItems'][$p]['FTPunCode'];
+				$tSplCode = $aPdtList['raItems'][$p]['FTSplCode'];
+				$nPdtCost = $aPdtList['raItems'][$p]['FCPdtCostAFDis'];
+				$nPdtUnitPri = $aPdtList['raItems'][$p]['FCPdtNetSalPri'];
+				$tPunName = $aPdtList['raItems'][$p]['FTPunName'];
 
 
-                      $aItemsInfo = array("tPdtCode"=>$tPdtCode,
-                                          "tPdtName"=>$tPdtName,
-                                          "tPunCode"=>$tPunCode,
-                                          "tPunName"=>$tPunName,
-                                          "tSplCode"=>$tSplCode,
-                                          "nPdtCost"=>$nPdtCost,
-                                          "nPdtUnitPri"=>$nPdtUnitPri);
+				$aItemsInfo = array(
+					"tPdtCode" => $tPdtCode,
+					"tPdtName" => $tPdtName,
+					"tPunCode" => $tPunCode,
+					"tPunName" => $tPunName,
+					"tSplCode" => $tSplCode,
+					"nPdtCost" => $nPdtCost,
+					"nPdtUnitPri" => $nPdtUnitPri
+				);
 
-                      $tItemInfo = json_encode($aItemsInfo);
+				$tItemInfo = json_encode($aItemsInfo);
 
-                ?>
-                <div class="col-sm-3 col-md-3 col-lg-3">
-                    <div class="thumbnail" data-iteminfo='<?=$tItemInfo?>' onclick="FSvQUOAddItemToTemp(this)">
-                        <img src="<?=base_url('application/assets/images/products/NoImage.png') ?>" alt="...">
-                        <div class="caption">
-                            <h4><?php echo $tPdtName;?></h4>
-                            <span>&#3647;<?php echo number_format($nPdtUnitPri,2);?></span>
-                        </div>
-                    </div>
-                </div>
-                <?php } //End for?>
+			?>
+			<div class="col-sm-3 col-md-3 col-lg-3">
+				<div class="thumbnail" data-iteminfo='<?= $tItemInfo ?>' onclick="FSvQUOAddItemToTemp(this)">
+					<img src="<?= base_url('application/assets/images/products/NoImage.png') ?>" alt="...">
+					<div class="caption">
+						<h4><?php echo $tPdtName; ?></h4>
+						<span>&#3647;<?php echo number_format($nPdtUnitPri, 2); ?></span>
+					</div>
+				</div>
+			</div>
+			<?php } ?>
+		<?php } else { ?>
+			<table class="table table-striped xCNTableCenter">
+				<thead>
+					<tr>
+						<th style="">ชื่อ</th>
+						<th style="width:250px; text-align: right;">ราคา/หน่วย</th>
+						<th style="width:100px;">เลือก</th>
+					</tr>
+				</thead>
+				<?php for($p2 = 0; $p2 < $nTotalRecord; $p2++) {
+							$tPdtCode 		= $aPdtList['raItems'][$p2]['FTPdtCode'];
+							$tPdtName 		= $aPdtList['raItems'][$p2]['FTPdtName'];
+							$tPunCode 		= $aPdtList['raItems'][$p2]['FTPunCode'];
+							$tSplCode 		= $aPdtList['raItems'][$p2]['FTSplCode'];
+							$nPdtCost 		= $aPdtList['raItems'][$p2]['FCPdtCostAFDis'];
+							$nPdtUnitPri 	= $aPdtList['raItems'][$p2]['FCPdtNetSalPri'];
+							$tPunName 		= $aPdtList['raItems'][$p2]['FTPunName'];
+							$aItemsInfo = array(
+								"tPdtCode" 		=> $tPdtCode,
+								"tPdtName" 		=> $tPdtName,
+								"tPunCode" 		=> $tPunCode,
+								"tPunName" 		=> $tPunName,
+								"tSplCode" 		=> $tSplCode,
+								"nPdtCost" 		=> $nPdtCost,
+								"nPdtUnitPri" => $nPdtUnitPri
+							);
+						$tItemInfo = json_encode($aItemsInfo); ?>
+					<tr>
+						<td style="text-align: left;"><?=$tPdtName;?></td>
+						<td style="text-align: right;"><?=number_format($nPdtUnitPri,2);?></td>
+						<td><button class="sm-button" data-iteminfo='<?= $tItemInfo ?>' onclick="FSvQUOAddItemToTemp(this)">เลือก</button></td>
+					</tr>
+				<?php } ?>
+			</table>
+	<?php } ?>
 <?php } else { ?>
- <table class="table table-striped">
-   <tr>
-     <th>ชื่อ</th>
-     <th>ราคา/หน่วย</th>
-     <th>เลือก</th>
-   </tr>
-   <?php for($p2 = 0 ;$p2 < $nTotalRecord; $p2++){
-         $tPdtCode = $aPdtList['raItems'][$p2]['FTPdtCode'];
-         $tPdtName = $aPdtList['raItems'][$p2]['FTPdtName'];
-         $tPunCode = $aPdtList['raItems'][$p2]['FTPunCode'];
-         $tSplCode = $aPdtList['raItems'][$p2]['FTSplCode'];
-         $nPdtCost = $aPdtList['raItems'][$p2]['FCPdtCostAFDis'];
-         $nPdtUnitPri = $aPdtList['raItems'][$p2]['FCPdtNetSalPri'];
-         $tPunName = $aPdtList['raItems'][$p2]['FTPunName'];
-
-         $aItemsInfo = array("tPdtCode"=>$tPdtCode,
-                             "tPdtName"=>$tPdtName,
-                             "tPunCode"=>$tPunCode,
-                             "tPunName"=>$tPunName,
-                             "tSplCode"=>$tSplCode,
-                             "nPdtCost"=>$nPdtCost,
-                             "nPdtUnitPri"=>$nPdtUnitPri);
-
-         $tItemInfo = json_encode($aItemsInfo);
-   ?>
-   <tr>
-     <td><?=$tPdtName?></td>
-     <td>฿ 3,955.00</td>
-     <td><button class="sm-button" data-iteminfo='<?=$tItemInfo?>' onclick="FSvQUOAddItemToTemp(this)">เลือก</button></td>
-   </tr>
-   <?php }?>
- </table>
+	<div class="col-lg-12">
+		<lable style="color:red">[การแจ้งเตือน]</label> ไม่พบสินค้าในระบบ.
+	</div>
 <?php } ?>
-<?php }else {?>
-       <div class="col-lg-12"><lable style="color:red">[การแจ้งเตือน]</label> ไม่พบสินค้าในระบบ.</div>
-<?php } //end main if?>
 
 <style media="screen">
-.thumbnail:hover {
-  padding: 2px;
-  border : 1px solid green !important;
-  border-radius: 5px !important;
-  cursor: pointer !important;
-}
+	.thumbnail:hover {
+		padding: 2px;
+		border: 1px solid green !important;
+		border-radius: 5px !important;
+		cursor: pointer !important;
+	}
 </style>
