c623b5a4ed5df85c4df789e3568978949ce64307
Supawat Wat. Putayanan <Supawat@adasoft.adasoft.com>
2020-04-25 18:18:02 +0700

    permission

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','form','rowlength','processcost');
+$autoload['helper'] = array('url','form','rowlength','processcost','permission');
 
 /*
 | -------------------------------------------------------------------
