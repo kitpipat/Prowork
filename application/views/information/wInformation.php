0416f524cb08956bb15a66b4ea92d93dcb321a23
Supawat Wat. Putayanan <Supawat@adasoft.adasoft.com>
2020-04-30 23:34:37 +0700

    wInformation

--- application/views/information/wInformation.php
+++ application/views/information/wInformation.php
@@ -1,75 +1,8 @@
-<style>
 
-	.tab_container {
-		margin		: 0 auto;
-		position	: relative;
-	}
-
-	input, section {
-		clear		: both;
-		padding-top	: 10px;
-		display		: none;
-	}
-
-	label {
-		display	: block;
-		float	: left;
-		width	: 15%;
-		padding	: 0.65em;
-		color	: #757575;
-		cursor	: pointer;
-		text-decoration: none;
-		text-align		: center;
-		background	: #f0f0f0;
-	}
-
-	#tab1:checked ~ #content1,
-	#tab2:checked ~ #content2,
-	#tab3:checked ~ #content3 {
-		display			: block;
-		padding			: 20px;
-		background		: #fff;
-		border-bottom	: 2px solid #f0f0f0;
-	}
-
-	.tab_container .tab-content h3  {
-		text-align	: center;
-	}
-
-	.tab_container [id^="tab"]:checked + label {
-		background	: #fff;
-		box-shadow	: inset 0 3px #51c448;
-	}
-
-	.tab_container [id^="tab"]:checked + label .fa {
-		color		: #51c448;
-	}
-
-	label .fa {
-		font-size	: 12.5px;
-		margin		: 0 0.6em 0 0;
-	}
-
-	.xCNImgCenter{
-		width	:	200px !important;
-		height	:	200px !important;
-	}
-
-	.ospValue{
-		color: #5a5a5a;
-	}
-
-</style>
 
 <div class="container-fulid">
 
-	<!--Section บน-->
-	<div class="row">
-		<div class="col-lg-6 col-md-6"><span class="xCNHeadMenu">บัญชีส่วนตัว</span></div>
-	</div>
-
 	<!--Section ล่าง-->
-
 	<div class="row">
 
 		<div class="col-lg-3" style="margin-top: 10px;">
@@ -156,100 +89,64 @@
 			<div class="card">
 				<div class="card-body">
 
-					<!--tab-->
-					<div class="row">
-						<div class="col-lg-12">
-							<div id="odvContent_Information" class="xCNContent">
+					<div class="container demo">
 
-								<div class="tab_container">
-									<input id="tab1" type="radio" name="tabs" checked>
-									<label for="tab1"><i class="fa fa-info"></i><span>รายละเอียด</span></label>
-
-									<input id="tab2" type="radio" name="tabs">
-									<label for="tab2"><i class="fa fa-cog"></i><span>เปลี่ยนรหัสผ่าน</span></label>
-
-									<input id="tab3" type="radio" name="tabs">
-									<label for="tab3"><i class="fa fa-file-text-o"></i><span>ประวัติเอกสาร</span></label>
-
-									<!--รายละเอียด-->
-									<section id="content1" class="tab-content">
-										<div class="row">
-
-											<div class="col-lg-3">
-												<?php 
-													$FTUsrImgPath = $aGetInfomation[0]['FTUsrImgPath'];
-													if(@$FTUsrImgPath != '' || @$FTUsrImgPath != null){
-														$tPathImage = './application/assets/images/user/'.@$FTUsrImgPath;
-														if (file_exists($tPathImage)){
-															$tPathImage = base_url().'application/assets/images/user/'.@$FTUsrImgPath;
-														}else{
-															$tPathImage = base_url().'application/assets/images/user/NoImage.png';
-														}
-													}else{
-														$tPathImage = './application/assets/images/user/NoImage.png';
-													}
-												?>
-												<img id="oimImgInsertorEditUser" class="img-responsive xCNImgCenter" src="<?=$tPathImage;?>">
-											</div>
-											<div class="col-lg-6">
-												<span>คุณ </span><span><?=$aGetInfomation[0]['FTUsrFName'] . ' ' . $aGetInfomation[0]['FTUsrLName'];?></span><br>
-												<div class="row">
-													<div class="col-lg-2"><span>สาขา : </span></div>
-													<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTBchName'] == '') ? 'ไม่ระบุสาขา' : $aGetInfomation[0]['FTBchName'];?></span></div>
-
-													<div class="col-lg-2"><span>แผนก : </span></div>
-													<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTUsrDep'] == '') ? 'ไม่พบแผนก' : $aGetInfomation[0]['FTUsrDep'];?></span></div>
-
-													<div class="col-lg-2"><span>กลุ่มสิทธิ์ : </span></div>
-													<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTRhdName'] == '') ? 'ไม่พบกลุ่มสิทธิ์' : $aGetInfomation[0]['FTRhdName'];?></span></div>
-
-													<div class="col-lg-2"><span>กลุ่มราคา : </span></div>
-													<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTPriGrpName'] == '') ? 'ไม่พบกลุ่มราคา' : $aGetInfomation[0]['FTPriGrpName'];?></span></div>
-
-													<div class="col-lg-2"><span>หมายเหตุ : </span></div>
-													<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTUsrRmk'] == '') ? '-' : $aGetInfomation[0]['FTUsrRmk'];?></span></div>
-													
-													<div class="col-lg-12" style="margin-top: 25px;">
-														<button class="xCNButtonSave" onclick="JSxEventSaveorEdit('r_adjcosteventinsert');">แก้ไขข้อมูล</button>
-													</div>
-												</div>
-											</div>
-										</div>
-										
-									</section>
-
-									<!--เปลี่ยนรหัสผ่าน-->
-									<section id="content2" class="tab-content">
-										<h3>Headline 2</h3>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
-										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-									</section>
-
-									<!--ประวัติเอกสาร-->
-									<section id="content3" class="tab-content">
-										
-									</section>
+						<div class="accordion" id="accordionExample">
+							<div class="card">
+								<div class="card-header" id="headingOne">
+								<h2 class="mb-0">
+									<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+									Collapsible Group Item #1
+									</button>
+								</h2>
 								</div>
 
+								<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+								<div class="card-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+								</div>
+							</div>
+							<div class="card">
+								<div class="card-header" id="headingTwo">
+								<h2 class="mb-0">
+									<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+									Collapsible Group Item #2
+									</button>
+								</h2>
+								</div>
+								<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+								<div class="card-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+								</div>
+							</div>
+							<div class="card">
+								<div class="card-header" id="headingThree">
+								<h2 class="mb-0">
+									<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+									Collapsible Group Item #3
+									</button>
+								</h2>
+								</div>
+								<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+								<div class="card-body">
+									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								</div>
+								</div>
 							</div>
 						</div>
+
 					</div>
+
 				</div>
 			</div>
 		</div>
 	</div>	
-<div>
+</div>
 
 <script>  
 	//ตัวเลขวิ่งเอง
-	JSxModalProgress('close');
 	$('.count').each(function() {
         $(this).prop('Counter', 0).animate({
             Counter: $(this).text()
@@ -260,5 +157,5 @@
                 $(this).text(Math.ceil(now));
             }
         });
-    });
+	});
 </script>
