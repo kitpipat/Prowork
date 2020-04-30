c100fe07392bcabd76d8c16ee49e7d58e5cde48e
Supawat Wat. Putayanan <Supawat@adasoft.adasoft.com>
2020-05-01 00:17:24 +0700

    information

--- application/views/information/wInformation.php
+++ application/views/information/wInformation.php
@@ -1,7 +1,12 @@
-
+<style>
+	.card-header{
+		background-color: #67717d;
+		height: 40px;
+		padding: 0rem 0.75rem;
+	}
+</style>
 
 <div class="container-fulid">
-
 	<!--Section ล่าง-->
 	<div class="row">
 
@@ -86,62 +91,84 @@
 		</div>
 
 		<div class="col-lg-9" style="margin-top: 10px;">
-			<div class="card">
-				<div class="card-body">
+			<div class="accordion" id="accordionExample">
+				
+				<!--ข้อมูลผู้ใช้-->
+				<div class="card">
+					<div class="card-header">
+						<h2 class="mb-0">
+							<button style="margin-top: -8px; color: #FFF;" class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+								ข้อมูลผู้ใช้
+							</button>
+						</h2>
+					</div>
 
-					<div class="container demo">
-
-						<div class="accordion" id="accordionExample">
-							<div class="card">
-								<div class="card-header" id="headingOne">
-								<h2 class="mb-0">
-									<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-									Collapsible Group Item #1
-									</button>
-								</h2>
+					<div id="collapseOne" class="collapse show" data-parent="#accordionExample">
+						<div class="card-body">
+							<div class="row">
+									
+								<div class="col-lg-12" style="margin-top: 0px;">
+									<button class="xCNButtonSave pull-right" onclick="JSxEventSaveorEdit('r_adjcosteventinsert');">แก้ไขข้อมูล</button>
 								</div>
 
-								<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-								<div class="card-body">
-									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+								<div class="col-lg-4">
+									<?php 
+										$FTUsrImgPath = $aGetInfomation[0]['FTUsrImgPath'];
+										if(@$FTUsrImgPath != '' || @$FTUsrImgPath != null){
+											$tPathImage = './application/assets/images/user/'.@$FTUsrImgPath;
+											if (file_exists($tPathImage)){
+												$tPathImage = base_url().'application/assets/images/user/'.@$FTUsrImgPath;
+											}else{
+												$tPathImage = base_url().'application/assets/images/user/NoImage.png';
+											}
+										}else{
+											$tPathImage = './application/assets/images/user/NoImage.png';
+										}
+									?>
+									<img id="oimImgInformation" class="img-responsive xCNImgCenter" src="<?=$tPathImage;?>">
 								</div>
-								</div>
-							</div>
-							<div class="card">
-								<div class="card-header" id="headingTwo">
-								<h2 class="mb-0">
-									<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-									Collapsible Group Item #2
-									</button>
-								</h2>
-								</div>
-								<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-								<div class="card-body">
-									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-								</div>
-								</div>
-							</div>
-							<div class="card">
-								<div class="card-header" id="headingThree">
-								<h2 class="mb-0">
-									<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-									Collapsible Group Item #3
-									</button>
-								</h2>
-								</div>
-								<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-								<div class="card-body">
-									Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-								</div>
+								<div class="col-lg-8">
+									<span>คุณ </span><span><?=$aGetInfomation[0]['FTUsrFName'] . ' ' . $aGetInfomation[0]['FTUsrLName'];?></span><br>
+									<div class="row">
+										<div class="col-lg-2"><span>สาขา : </span></div>
+										<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTBchName'] == '') ? 'ไม่ระบุสาขา' : $aGetInfomation[0]['FTBchName'];?></span></div>
+
+										<div class="col-lg-2"><span>แผนก : </span></div>
+										<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTUsrDep'] == '') ? 'ไม่พบแผนก' : $aGetInfomation[0]['FTUsrDep'];?></span></div>
+
+										<div class="col-lg-2"><span>กลุ่มสิทธิ์ : </span></div>
+										<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTRhdName'] == '') ? 'ไม่พบกลุ่มสิทธิ์' : $aGetInfomation[0]['FTRhdName'];?></span></div>
+
+										<div class="col-lg-2"><span>กลุ่มราคา : </span></div>
+										<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTPriGrpName'] == '') ? 'ไม่พบกลุ่มราคา' : $aGetInfomation[0]['FTPriGrpName'];?></span></div>
+
+										<div class="col-lg-2"><span>หมายเหตุ : </span></div>
+										<div class="col-lg-9"><span class="ospValue"><?=($aGetInfomation[0]['FTUsrRmk'] == '') ? '-' : $aGetInfomation[0]['FTUsrRmk'];?></span></div>
+									</div>
 								</div>
 							</div>
 						</div>
-
 					</div>
+				</div>
 
+				<!--ประวัติการทำการเอกสาร-->
+				<div class="card">
+					<div class="card-header">
+					<h2 class="mb-0">
+						<button style="margin-top: -8px; color: #FFF;" class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+							ประวัติการทำเอกสาร
+						</button>
+					</h2>
+					</div>
+					<div id="collapseTwo" class="collapse" data-parent="#accordionExample">
+					<div class="card-body">
+						S
+					</div>
+					</div>
 				</div>
 			</div>
 		</div>
+
 	</div>	
 </div>
 
@@ -157,5 +184,5 @@
                 $(this).text(Math.ceil(now));
             }
         });
-	});
+    });
 </script>
