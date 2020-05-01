37bf37f469e6b6b5a6fbe40773dbb516ce782535
Kitpipat <kitpipat>
2020-05-01 12:16:40 +0700

    เพิ่มฟังก์ชั่นการคำนวนส่วนลดแบบ step

--- application/helpers/calculatecost_helper.php
+++ application/helpers/calculatecost_helper.php
@@ -1,19 +1,56 @@
 <?php
-  
-    function FCNtCOSCalCost($paPdtInfo){
 
-             $nStdCost =    $paPdtInfo['nStdCost'];
-             $tDisCost =    $paPdtInfo['tDisCost'];
-             if($tDisCost !='' or $tDisCost !=0){
+    function FCNnHCOSCalCost($paPdtInfo){
 
-                $tDisCost = str_replace("%","%",$tDisCost);
-                $aDisCost = explode(",",$tDisCost);
-                var_dump($aDisCost);
+             //ตรวจสอบว่ามีการส่งค่าสำหรับการคำนวนมาหรือไม่
+             if(count($paPdtInfo) > 0){
 
+                 $nStdCost =    $paPdtInfo['nStdCost'];
+                 $tDisCost =    $paPdtInfo['tStepDisCost'];
+
+                 //ตรวจสอบว่ามีการส่งต้นทุนสำหรับการคำนวนมาหรือไม่
+                 if($nStdCost == '' or $nStdCost == null){
+                    $nStdCost = 0;
+                 }
+
+                 //ตรวจสอบว่ามีการกรอกส่วนลดมาหรือไม่
+                 if($tDisCost !='' or $tDisCost !=0){
+
+                    $aDisCost = explode(",",$tDisCost);
+                    $nCostAFDis = $nStdCost;
+                    for($i=0;$i<count($aDisCost);$i++){
+
+                        $tDisCost = trim(str_replace("%","",$aDisCost[$i]));
+
+                        //หาว่าเป็นการลดแบบไหน ลดบาท หรือ ลด %
+                        $tDisType = substr($aDisCost[$i],strlen($aDisCost[$i]) -1);
+
+                        //ตรวจสอบว่ามีการกรอกส่วนลดที่ถูกต้องตาม Format หรือไม่ ถ้าไม่ถูกระบบจะไม่คำนวนส่วนลดให้
+                        if($tDisType !='%' and !is_numeric($tDisType)){
+
+                           $nCostAFDis  = $nCostAFDis;
+
+                        }else{
+                                //การกรอกส่วนลดถูกต้องตาม format ให้ทำการคำนวนตามประเภทส่วนลด
+                                if($tDisType == '%'){
+                                   $nCostAFDis = $nCostAFDis - (($nCostAFDis * $tDisCost)/100); //ลดเป็นเปอร์เซนต์
+                                }else{
+                                   $nCostAFDis = $nCostAFDis - (-$tDisCost); //ลดเป็นบาท
+                                }
+                        }
+
+                    }
+
+                      return $nCostAFDis;
+
+                 }else{
+
+                    return $nStdCost;
+
+                 }
              }else{
-
-                return $nStdCost;
-
+                  //ไม่มีการส่งค่าสำหรับคำนวนมา
+                  return 0;
              }
 
     }
