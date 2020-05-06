d6497a46abdd940f65efbb03500368351fbbfd7c
Kitpipat <kitpipat>
2020-05-07 02:43:03 +0700

    text

--- application/views/information/wInformation.php
+++ application/views/information/wInformation.php
@@ -206,7 +206,7 @@
 
 								<div class="col-lg-6">
 									<div class="input-group md-form form-sm form-2 pl-0">
-										<input class="form-control my-0 py-1 red-border xCNFormSerach" type="text" placeholder="ค้ค้นหาจากเลขที่เอกสาร" id="oetSearch" onkeypress="Javascript:if(event.keyCode==13) JSwLoadTableList(1)">
+										<input class="form-control my-0 py-1 red-border xCNFormSerach" type="text" placeholder="ค้นหาจากเลขที่เอกสาร" id="oetSearch" onkeypress="Javascript:if(event.keyCode==13) JSwLoadTableList(1)">
 										<div class="input-group-append">
 											<span class="input-group-text red lighten-3" style="cursor:pointer;" onclick="JSwLoadTableList(1);"><i class="fa fa-search" aria-hidden="true"></i></span>
 										</div>
