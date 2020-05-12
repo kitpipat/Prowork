027105648c812325bc8ca5337f584636211c74bb
Kitpipat <kitpipat>
2020-05-13 00:50:52 +0700

    add version

--- application/views/login/wLogin.php
+++ application/views/login/wLogin.php
@@ -22,6 +22,7 @@
 
 					<div id="odvContentLogin">
 						<img id="oimLogin" src="<?=base_url('application/assets/')?>images/logo.jpg">
+            <p style="text-align:center;color:green">Version 0.2.0-13052020</p>
 						<form id="ofmLogin" class="form-signin" method="post">
 							<div class="input">
 								<label class="olbLabelLogin">ชื่อผู้ใช้</label>
@@ -123,7 +124,7 @@
 						cache	: false,
 						timeout	: 0,
 						success	: function (tResult) {
-          
+
 							var aReturn = JSON.parse(tResult);
 							if(aReturn.rtCode == '800'){
 								$('#odvCheckProgress').fadeOut("slow");
