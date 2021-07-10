03ed399ca8ce1ba8cb38e875911581a0c72219d4
Supawat <supawat@ada-soft.com>
2021-07-10 18:22:58 +0700

    wLogin

--- application/views/login/wLogin.php
+++ application/views/login/wLogin.php
@@ -22,7 +22,7 @@
 
 					<div id="odvContentLogin">
 						<img id="oimLogin" src="<?=base_url('application/assets/')?>images/logo.jpg">
-            			<p style="text-align:center;color:green">Version 0.7.8-05072021</p>
+            			<p style="text-align:center;color:green">Version 0.7.8-10072021</p>
 						<form id="ofmLogin" class="form-signin" method="post">
 							<div class="input">
 								<label class="olbLabelLogin">ชื่อผู้ใช้</label>
